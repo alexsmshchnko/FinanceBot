89390a9b85596fcd93ad46cc90abcc8acfdfe285
Aleksandr Semashchenko <asemashchenko@openintegration.inc>
2023-09-17 11:52:03 +0300

    feat: init dialogs and balance

---
+++ cmd/main.go
@@ -0,0 +1,252 @@
+package main
+
+import (
+	"financebot/internal"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+var gBot *tgbotapi.BotAPI
+var gToken string
+var gChatId int64
+var gUsersInChat Users
+var gUsefullActivities = Activities{
+	// Self-Development
+	{"yoga", "Yoga (15 minutes)", 1},
+	{"meditation", "Meditation (15 minutes)", 1},
+	{"language", "Learning a foreign language (15 minutes)", 1},
+	{"swimming", "Swimming (15 minutes)", 1},
+	{"walk", "Walk (15 minutes)", 1},
+	{"chores", "Chores", 1},
+
+	// Work
+	{"work_learning", "Studying work materials (15 minutes)", 1},
+	{"portfolio_work", "Working on a portfolio project (15 minutes)", 1},
+	{"resume_edit", "Resume editing (15 minutes)", 1},
+
+	// Creativity
+	{"creative", "Creative creation (15 minutes)", 1},
+	{"reading", "Reading fiction literature (15 minutes)", 1},
+}
+var gRewards = Activities{
+	// Entertainment
+	{"watch_series", "Watching a series (1 episode)", 10},
+	{"watch_movie", "Watching a movie (1 item)", 30},
+	{"social_nets", "Browsing social networks (30 minutes)", 10},
+
+	// Food
+	{"eat_sweets", "300 kcal of sweets", 60},
+}
+
+type User struct {
+	id    int64
+	name  string
+	coins uint16
+}
+
+type Activity struct {
+	code, name string
+	coins      uint16
+}
+
+type Activities []*Activity
+
+type Users []*User
+
+func init() {
+	// Uncomment and update token value to set environment variable for Telegram Bot Token given by BotFather.
+	// Delete this line after setting the env var. Keep the token out of the public domain!
+	//_ = os.Setenv(TokenNameOnOS, "INSERT_YOUR_TOKEN")
+
+	if gToken = os.Getenv(internal.TokenNameOnOS); gToken == "" {
+		panic(fmt.Errorf("failed to load env variable %s", internal.TokenNameOnOS))
+	}
+
+	var err error
+	if gBot, err = tgbotapi.NewBotAPI(gToken); err != nil {
+		log.Panic(err)
+	}
+
+	gBot.Debug = true
+}
+
+func isCallbackQueryNil(update *tgbotapi.Update) bool {
+	return update.CallbackQuery != nil && update.CallbackQuery.Data != ""
+}
+
+func showMenu() {
+	msg := tgbotapi.NewMessage(gChatId, "Choose option")
+	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+		getKeyboardRow(internal.BUTTON_TEXT_BALANCE, internal.BUTTON_CODE_BALANCE),
+		getKeyboardRow(internal.BUTTON_TEXT_USEFUL_ACTIVITIES, internal.BUTTON_CODE_USEFUL_ACTIVITIES),
+		getKeyboardRow(internal.BUTTON_TEXT_REWARDS, internal.BUTTON_CODE_REWARDS),
+	)
+
+	gBot.Send(msg)
+}
+
+func showBalance(user *User) {
+	msg := fmt.Sprintf("%s, wallet is empty now %s\n track activity to get some coins", user.name, internal.EMOJI_DONT_KNOW)
+	if coins := user.coins; coins > 0 {
+		msg = fmt.Sprintf("%s, you have %d %s", user.name, internal.EMOJI_COIN)
+	}
+
+	gBot.Send(tgbotapi.NewMessage(gChatId, msg))
+
+	showMenu()
+}
+
+func callbackQueryIsMissing(update *tgbotapi.Update) bool {
+	return update.CallbackQuery == nil || update.CallbackQuery.From == nil
+}
+
+func getUserFromUpdate(update *tgbotapi.Update) (user *User, found bool) {
+	if callbackQueryIsMissing(update) {
+		return
+	}
+
+	userId := update.CallbackQuery.From.ID
+	for _, userInChat := range gUsersInChat {
+		if userId == userInChat.id {
+			return userInChat, true
+		}
+	}
+	return
+}
+
+func saveUserFromUpdate(update *tgbotapi.Update) (user *User, found bool) {
+	if callbackQueryIsMissing(update) {
+		return
+	}
+
+	from := update.CallbackQuery.From
+
+	user = &User{
+		id:    from.ID,
+		name:  strings.TrimSpace(from.FirstName + " " + from.LastName),
+		coins: 0,
+	}
+
+	gUsersInChat = append(gUsersInChat, user)
+	return user, true
+}
+
+func showActivities(activities Activities, text string, isUsefull bool) {
+	activitiesButtonRows := make([]([]tgbotapi.InlineKeyboardButton), 0, len(activities)+1)
+	for _, activity := range activities {
+		activitiesButtonRows = append(activitiesButtonRows, getKeyboardRow(activity.name, activity.code))
+	}
+	activitiesButtonRows = append(activitiesButtonRows, getKeyboardRow(internal.BUTTON_TEXT_PRINT_MENU, internal.BUTTON_CODE_PRINT_MENU))
+
+	msg := tgbotapi.NewMessage(gChatId, text)
+	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(activitiesButtonRows...)
+	gBot.Send(msg)
+}
+
+func showUsefulActivities() {
+	showActivities(gUsefullActivities, "Options to track your activity", true)
+}
+
+func showRewards() {
+	showActivities(gRewards, "Pick your reward", false)
+}
+
+func updateProcessing(update *tgbotapi.Update) {
+	user, found := getUserFromUpdate(update)
+	if !found {
+		if user, found = saveUserFromUpdate(update); !found {
+			gBot.Send(tgbotapi.NewMessage(gChatId, "User identification failed"))
+			return
+		}
+	}
+	choiseCode := update.CallbackQuery.Data
+	log.Printf("[&T] %s", time.Now(), choiseCode)
+
+	switch choiseCode {
+	case internal.BUTTON_CODE_BALANCE:
+		showBalance(user)
+	case internal.BUTTON_CODE_USEFUL_ACTIVITIES:
+		showUsefulActivities()
+	case internal.BUTTON_CODE_REWARDS:
+		showRewards()
+	case internal.BUTTON_CODE_PRINT_INTRO:
+		printIntro(update)
+		showMenu()
+	case internal.BUTTON_CODE_SKIP_INTRO:
+		showMenu()
+	}
+
+}
+
+func isStartMessage(update *tgbotapi.Update) bool {
+	return update.Message != nil && update.Message.Text == "/start"
+}
+
+func delay(seconds uint8) {
+	time.Sleep(time.Second * time.Duration(seconds))
+}
+
+func sendMessageWithDelay(delayInSec uint8, message string) {
+	gBot.Send(tgbotapi.NewMessage(gChatId, message))
+	delay(delayInSec)
+}
+
+func printIntro(update *tgbotapi.Update) {
+	sendMessageWithDelay(2, "Hello! "+internal.EMOJI_SUNGLASSES)
+	sendMessageWithDelay(7, "There are numerous beneficial actions that, by performing regularly, we improve the quality of our life. However, often it's more fun, easier, or tastier to do something harmful. Isn't that so?")
+	sendMessageWithDelay(7, "With greater likelihood, we'll prefer to get lost in YouTube Shorts instead of an English lesson, buy M&M's instead of vegetables, or lie in bed instead of doing yoga.")
+	sendMessageWithDelay(1, internal.EMOJI_SAD)
+	sendMessageWithDelay(10, "Everyone has played at least one game where you need to level up a character, making them stronger, smarter, or more beautiful. It's enjoyable because each action brings results. In real life, though, systematic actions over time start to become noticeable. Let's change that, shall we?")
+	sendMessageWithDelay(1, internal.EMOJI_SMILE)
+	sendMessageWithDelay(14, `Before you are two tables: "Useful Activities" and "Rewards". The first table lists simple short activities, and for completing each of them, you'll earn the specified amount of coins. In the second table, you'll see a list of activities you can only do after paying for them with coins earned in the previous step.`)
+	sendMessageWithDelay(1, internal.EMOJI_COIN)
+	sendMessageWithDelay(10, `For example, you spend half an hour doing yoga, for which you get 2 coins. After that, you have 2 hours of programming study, for which you get 8 coins. Now you can watch 1 episode of "Interns" and break even. It's that simple!`)
+	sendMessageWithDelay(6, `Mark completed useful activities to not lose your coins. And don't forget to "purchase" the reward before actually doing it.`)
+}
+
+func getKeyboardRow(buttonText, buttonCode string) []tgbotapi.InlineKeyboardButton {
+	return tgbotapi.NewInlineKeyboardRow(tgbotapi.NewInlineKeyboardButtonData(buttonText, buttonCode))
+}
+
+func askToPrintIntro() {
+	msg := tgbotapi.NewMessage(gChatId, "Do you want to get my intro?")
+	msg.ReplyMarkup = tgbotapi.NewInlineKeyboardMarkup(
+		getKeyboardRow(internal.BUTTON_TEXT_PRINT_INTRO, internal.BUTTON_CODE_PRINT_INTRO),
+		getKeyboardRow(internal.BUTTON_TEXT_SKIP_INTRO, internal.BUTTON_CODE_SKIP_INTRO),
+	)
+
+	gBot.Send(msg)
+}
+
+func run() (err error) {
+	log.Printf("Authorized on account %s", gBot.Self.UserName)
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = internal.UpdateConfigTimeout
+
+	for update := range gBot.GetUpdatesChan(updateConfig) {
+		if isCallbackQueryNil(&update) {
+			updateProcessing(&update)
+		} else if isStartMessage(&update) {
+			log.Printf("Reply: [%s] %s", update.Message.From.UserName, update.Message.Text)
+			gChatId = update.Message.Chat.ID
+
+			askToPrintIntro()
+		}
+
+	}
+
+	return err
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		log.Fatal(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module financebot
 
-go 1.21.1
+//go 1.21.1
+go 1.20
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
